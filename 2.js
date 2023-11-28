39b8154619f622ed95e0a6f5c4f182aa9b28e11f
Tushar Choudhari <tusharchoudhari88@gmail.com>
2023-11-28 10:49:11 +0530

    Commit on hotfix/1.0 branch

--- 2.js
+++ 2.js
@@ -1,4 +1,5 @@
 def getPerson(name,surname):
     /*Changing this to test git fetch and git merge*/
-
+    /* Changing this locally*/
+    /* changed using hotfix/1.0 branch*/ 
     return name+""+surname
